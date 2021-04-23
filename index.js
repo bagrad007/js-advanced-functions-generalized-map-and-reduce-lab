229ef99b7c1297e39cd042a51daba115a02e5fc8
Luke Gill <bagrad007@gmail.com>
2021-04-23 14:59:23 -0400

    commit tree f4840bae24b64cf847ef4dd2f608af56545d0676

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(source, callback) {
+    let arr = []
+
+    for (let i = 0; i < source.length; i++) {
+        let theElement = source[i]
+        arr.push(callback(theElement))
+    }
+
+    return arr;
+}
+
+
+function reduce(source, callback, int) {
+    let arr = (!!int) ? int : source[0]
+    let i = (!!int) ? 0 : 1
+
+    for (; i < source.length; i++) {
+        arr = callback(source[i], arr)
+    }
+    return arr
+}
